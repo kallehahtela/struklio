550bf03c4442115aa1ede658fa3351ee7b6cf4e7
kallehahtela <sk1mb4yt@gmail.com>
2025-03-27 21:31:45 +0200

    Expo router configure

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,11 +1,16 @@
 import { View, Text, StyleSheet, StatusBar, SafeAreaView } from "react-native";
-import HomeScreen from ".";
+import HomeScreen from "./index";
+import * as SystemUI from 'expo-system-ui';
 
 export default function RootLayout() {
+    SystemUI.setBackgroundColorAsync('white');
+
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <StatusBar />
-            <HomeScreen />
+            <View>
+                <HomeScreen />
+            </View>
         </SafeAreaView>
     );
 };
--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
     return (
-        <View>
-            <Text>This is a HomeScreen</Text>
+        <View style={{ justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{ color: 'black'}}>This is a HomeScreen</Text>
         </View>
     );
 };
